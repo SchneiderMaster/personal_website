59a535eecdf7c716670cfb4e1ac6b74e20d9dc2e
SchneiderMaster <fynn2746@outlook.de>
2024-03-29 20:46:22 +0100

    added custom button from github

---
+++ src/app/custom-button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './custom-button.module.css';
+
+export default function CustomButton() {
+    return (
+        <a className={styles.customButton} href='https://github.com/SchneiderMaster'>
+            
+        </a>
+    );
+}
