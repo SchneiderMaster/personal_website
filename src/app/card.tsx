07fd7e4270ff8e8c8134e0a712bdb9fc36bd21b8
SchneiderMaster <fynn2746@outlook.de>
2024-04-05 19:11:29 +0200

    added cool appear gradient slide thingy for the card

--- src/app/card.tsx
+++ src/app/card.tsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./card.module.css";
 
 export default function Card() {
 
 
+    const [isVisible, setIsVisible] = useState(false);
+    const targetRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      { threshold: 0.5 }
+    );
+
+    if (targetRef.current) {
+      observer.observe(targetRef.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+
     return(
-        <div className={styles.card}>
-            <div style={{backgroundImage: "url(channels4_profile.jpg)"}}></div>
-            <p>
+        <div className={`${styles.card} ${isVisible ? styles.visible : styles.invisible}`} style={{backgroundPositionX: isVisible ? styles.visible : styles.invisible}} ref={targetRef}>
+            <div className={styles.image} style={{backgroundImage: "url(channels4_profile.jpg)"}}></div>
+            <div className={styles.text}>
                 <h2>YouTube</h2>
                 YouTube is very cool and I am also very cool. Also, please subscribe and like because I am very cool and so is my video. Maybe, one day, I will be rich with this. Then, I wouldn't have to bother with doing stuff for other people that I don't care about.
-            </p>
+            </div>
         </div>
 
     )
