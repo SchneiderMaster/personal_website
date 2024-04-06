e45909d9d230c99c901c69c14d109a5edb0d7876
SchneiderMaster <fynn2746@outlook.de>
2024-04-06 16:11:22 +0200

    changed the card a little bit and tested out scroll()

--- src/app/card.tsx
+++ src/app/card.tsx
@@ -12,7 +12,7 @@ export default function Card() {
       ([entry]) => {
         setIsVisible(entry.isIntersecting);
       },
-      { threshold: 0.5 }
+      { threshold: 1 }
     );
 
     if (targetRef.current) {
@@ -29,7 +29,7 @@ export default function Card() {
         <div className={`${styles.card} ${isVisible ? styles.visible : styles.invisible}`} style={{backgroundPositionX: isVisible ? styles.visible : styles.invisible}} ref={targetRef}>
             <div className={styles.image} style={{backgroundImage: "url(channels4_profile.jpg)"}}></div>
             <div className={styles.text}>
-                <h2>YouTube</h2>
+                <h2 ref={targetRef} className={`${isVisible ? styles.fadedIn : styles.fadedOut}`}>YouTube</h2>
                 YouTube is very cool and I am also very cool. Also, please subscribe and like because I am very cool and so is my video. Maybe, one day, I will be rich with this. Then, I wouldn't have to bother with doing stuff for other people that I don't care about.
             </div>
         </div>
