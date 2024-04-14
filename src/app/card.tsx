c21cede2ae8d8d9feab8dc92f71bba4128ce92bf
SchneiderMaster <fynn2746@outlook.de>
2024-04-14 09:31:01 +0200

    fixed hydration errors

--- src/app/card.tsx
+++ src/app/card.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useRef, useState } from "react";
 import styles from "./card.module.css";
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,8 @@ import "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { collection, getDocs, getFirestore } from "firebase/firestore";
+import Script from "next/script";
+import React from "react";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -58,20 +60,20 @@ export default function RootLayout({
   return (
     <html lang="en">
     
-    <script defer src="/__/firebase/10.11.0/firebase-app-compat.js"></script>
-    <script defer src="/__/firebase/10.11.0/firebase-auth-compat.js"></script>
-    <script defer src="/__/firebase/10.11.0/firebase-database-compat.js"></script>
-    <script defer src="/__/firebase/10.11.0/firebase-firestore-compat.js"></script>
-    <script defer src="/__/firebase/10.11.0/firebase-functions-compat.js"></script>
-    <script defer src="/__/firebase/10.11.0/firebase-messaging-compat.js"></script>
-    <script defer src="/__/firebase/10.11.0/firebase-storage-compat.js"></script>
-    <script defer src="/__/firebase/10.11.0/firebase-analytics-compat.js"></script>
-    <script defer src="/__/firebase/10.11.0/firebase-remote-config-compat.js"></script>
-    <script defer src="/__/firebase/10.11.0/firebase-performance-compat.js"></script>
-    <script defer src="/__/firebase/init.js?useEmulator=true"></script>
+    <Script defer src="/__/firebase/10.11.0/firebase-app-compat.js"></Script>
+    <Script defer src="/__/firebase/10.11.0/firebase-auth-compat.js"></Script>
+    <Script defer src="/__/firebase/10.11.0/firebase-database-compat.js"></Script>
+    <Script defer src="/__/firebase/10.11.0/firebase-firestore-compat.js"></Script>
+    <Script defer src="/__/firebase/10.11.0/firebase-functions-compat.js"></Script>
+    <Script defer src="/__/firebase/10.11.0/firebase-messaging-compat.js"></Script>
+    <Script defer src="/__/firebase/10.11.0/firebase-storage-compat.js"></Script>
+    <Script defer src="/__/firebase/10.11.0/firebase-analytics-compat.js"></Script>
+    <Script defer src="/__/firebase/10.11.0/firebase-remote-config-compat.js"></Script>
+    <Script defer src="/__/firebase/10.11.0/firebase-performance-compat.js"></Script>
+    <Script defer src="/__/firebase/init.js?useEmulator=true"></Script>
 
 
-      <script src="https://apis.google.com/js/platform.js" async defer></script>
+      <Script src="https://apis.google.com/js/platform.js" async defer></Script>
       <meta name="google-signin-client_id" content="911079370304-2u99bp5ef59s8dl16a97gslt5dp7fgpm.apps.googleusercontent.com.apps.googleusercontent.com"></meta>
 
       <body className={inter.className}>{children}</body>
