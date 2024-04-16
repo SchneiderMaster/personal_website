c1621f8ddbf898fc26c7ef8fbeeb7842df5bbeca
SchneiderMaster <fynn2746@outlook.de>
2024-04-16 19:48:58 +0200

    destroyyyy the database

--- src/app/firestore.tsx
+++ src/app/firestore.tsx
@@ -1,122 +1,160 @@
-import { DocumentData, QueryDocumentSnapshot, QuerySnapshot, collection, doc, getDoc, getDocs, query, setDoc, where } from "firebase/firestore";
+import {
+	DocumentData,
+	QueryDocumentSnapshot,
+	QuerySnapshot,
+	collection,
+	doc,
+	getDoc,
+	getDocs,
+	query,
+	setDoc,
+	where,
+} from "firebase/firestore";
 import { app, auth, db } from "./firebase";
 import { getAuth, initializeAuth, onAuthStateChanged } from "firebase/auth";
 
+export async function createIssue(
+	title: string,
+	description: string,
+	projectId: string
+) {
+	try {
+		if (auth.currentUser) {
+			const projectSnapshot = await getDoc(
+				doc(db, "users", auth.currentUser.uid, "projects", projectId)
+			);
 
-export async function createIssue(title: string, description: string, projectId: string) {
+			if (projectSnapshot.exists()) {
+				const index = projectSnapshot.data().currentIndex;
 
-    try{
-        if(auth.currentUser){
+				await setDoc(
+					doc(
+						db,
+						"users",
+						auth.currentUser.uid,
+						"issues",
+						`${projectId}-${index + 1}`
+					),
+					{
+						title: title,
+						description: description,
+						projectId: projectId,
+					}
+				);
 
-            const projectSnapshot = await getDoc(doc(db, "users", auth.currentUser.uid, "projects", projectId));
+				const newProject = { ...projectSnapshot.data() };
+				newProject.currentIndex = index + 1;
 
-            if(projectSnapshot.exists()){
-                const index = projectSnapshot.data().currentIndex;
+				await setDoc(
+					doc(
+						db,
+						"users",
+						auth.currentUser.uid,
+						"projects",
+						projectId
+					),
+					newProject
+				);
+			} else {
+				console.log("First create a project and select it.");
+			}
+		} else {
+			console.log("You aren't signed in.");
+		}
+	} catch (err) {
+		console.log(auth.currentUser?.uid + "; " + auth);
 
-                await setDoc(doc(db, "users", auth.currentUser.uid, "issues", `${projectId}-${index+1}`), {
-                    title: title,
-                    description: description,
-                    projectId: projectId,
-                })
-
-                const newProject = {... projectSnapshot.data()}
-                newProject.currentIndex = index +1;
-
-                await setDoc(doc(db, "users", auth.currentUser.uid, "projects", projectId), newProject);
-
-            }
-            else{
-                console.log("First create a project and select it.");
-            }
-        }
-        else{
-            console.log("You aren't signed in.");
-        }
-    }
-    catch (err){
-        console.log(auth.currentUser?.uid + "; " + auth)
-
-        console.log(err);
-    }
-
+		console.log(err);
+	}
 }
 
-export async function getAllIssues(projectId: string){
-    try {
-        if(auth.currentUser) {
+export async function getAllIssues(projectId: string) {
+	try {
+		if (auth.currentUser) {
+			let querySnapshot: QuerySnapshot<DocumentData, DocumentData>;
+			console.log("testissues");
+			if (projectId === "") {
+				querySnapshot = await getDocs(
+					query(
+						collection(db, "users", auth.currentUser.uid, "issues")
+					)
+				);
+			} else {
+				console.log(projectId);
+				querySnapshot = await getDocs(
+					query(
+						collection(db, "users", auth.currentUser.uid, "issues"),
+						where("projectId", "==", projectId)
+					)
+				);
+			}
 
-            let querySnapshot: QuerySnapshot<DocumentData, DocumentData>;
+			const issues: QueryDocumentSnapshot[] = [];
 
-            if(projectId === ""){
-                querySnapshot = await getDocs(query(collection(db, "users", auth.currentUser.uid, "issues")))
-            }
-            
-            else{
-                console.log(projectId)
-                querySnapshot = await getDocs(query(collection(db, "users", auth.currentUser.uid, "issues"), where("projectId", "==", projectId)))
-            }
+			querySnapshot.forEach((doc) => {
+				console.log(doc.data().title);
+				issues.push(doc);
+			});
 
-
-            const issues: QueryDocumentSnapshot[] = [];
-
-            querySnapshot.forEach((doc) => {
-                console.log(doc.data().title)
-                issues.push(doc);
-            })
-
-            return issues;
-            
-        }else{
-            console.log("You aren't signed in.");
-            return null;
-        }
-
-    }
-
-    catch(err) {
-        console.log(err)
-        return null;
-    }
-
+			return issues;
+		} else {
+			console.log("You aren't signed in.");
+			return null;
+		}
+	} catch (err) {
+		console.log(err);
+		return null;
+	}
 }
 
-
-export async function createProject(projectId: string, projectName: string, projectDescription: string){
-    try{
-        if(auth.currentUser) {
-            await setDoc(doc(db, "users", auth.currentUser.uid, "projects", projectId), {
-                name: projectName,
-                description: projectDescription,
-                currentIndex: 0,
-            })
-    
-        }
-    }
-    catch(err) {
-        console.log(err);
-    }
-
+export async function createProject(
+	projectId: string,
+	projectName: string,
+	projectDescription: string
+) {
+	try {
+		if (auth.currentUser) {
+			await setDoc(
+				doc(db, "users", auth.currentUser.uid, "projects", projectId),
+				{
+					name: projectName,
+					description: projectDescription,
+					currentIndex: 0,
+				}
+			);
+		}
+	} catch (err) {
+		console.log(err);
+	}
 }
-
 
 export async function getAllProjects() {
-    try {
-        if(auth.currentUser) {
-            const querySnapshot = await getDocs(collection(db, "users", auth.currentUser.uid, "projects"));
+	try {
+		if (auth.currentUser) {
+			const querySnapshot = await getDocs(
+				collection(db, "users", auth.currentUser.uid, "projects")
+			);
+			console.log("test");
+			const projects: QueryDocumentSnapshot[] = [];
 
-            const projects: QueryDocumentSnapshot[] = [];
+			querySnapshot.forEach((doc) => {
+				projects.push(doc);
+			});
 
-            querySnapshot.forEach((doc) => {
-                projects.push(doc);
-            })
-
-            return projects;
-    }
+			return projects;
+		}
+	} catch (err) {
+		console.log(err);
+	}
 }
-    catch(err) {
-        console.log(err);
-    }
 
-
-
+export async function createWorklog(
+	issueId: string,
+	startDate: Date,
+	duration: number
+) {
+	try {
+	} catch (err) {
+		console.log(err);
+	}
 }
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,113 +1,249 @@
 import Navbar from "@/app/navbar";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "@/styles/globals.css";
 import Script from "next/script";
-import { createIssue, createProject, getAllIssues, getAllProjects } from "@/app/firestore";
+import {
+	createIssue,
+	createProject,
+	createWorklog,
+	getAllIssues,
+	getAllProjects,
+} from "@/app/firestore";
 import styles from "@/styles/test.module.css";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "@/app/firebase";
 import { QueryDocumentSnapshot, QuerySnapshot } from "firebase/firestore";
 
-export default function Test(){
+export default function Test() {
+	const [issues, setIssues] = useState<QueryDocumentSnapshot[]>([]);
 
-    let fetched = false;
+	const [projects, setProjects] = useState<QueryDocumentSnapshot[]>([]);
 
-    const [issues, setIssues] = useState<QueryDocumentSnapshot[]>([])
+	const [selectedProjectForSearch, setSelectedProjectForSearch] =
+		useState<string>("");
 
-    const [projects, setProjects] = useState<QueryDocumentSnapshot[]>([]);
+	const [selectedDate, setSelectedDate] = useState<Date>(new Date());
 
-    const [selectedProject, setSelectedProject] = useState<string>("");
+	const [selectedDuration, setSelectedDuration] = useState<number>(0);
 
-    const fetchIssues = async (projectId: string) => {
-        const snapshot = await getAllIssues(projectId);
-        if (snapshot) {
-            setIssues(snapshot);
-        }
-    };
+	const [issue, setIssue] = useState<string>("");
 
-    // onAuthStateChanged(auth, (user: any) => {if(!fetched) {fetchIssues(); fetched = true;}});
+	const fetchIssues = async (projectId: string) => {
+		const snapshot = await getAllIssues(projectId);
+		if (snapshot) {
+			setIssues(snapshot);
+		}
+	};
 
+	const fetchProjects = async () => {
+		const snapshot = await getAllProjects();
+		if (snapshot) {
+			setProjects(snapshot);
+		}
+	};
 
-    const fetchProjects = async () => {
-        const snapshot = await getAllProjects();
-        if (snapshot) {
-            setProjects(snapshot);
-        }
-    };
-    onAuthStateChanged(auth, (user: any) => {fetchProjects()});
+	useEffect(() => {
+		fetchProjects();
+	});
 
+	const [inputValues, setInputValues] = useState(["", "", ""]);
+	const [inputValues1, setInputValues1] = useState(["", "", ""]);
 
+	return (
+		<main className={styles.main}>
+			<Navbar></Navbar>
+			<h1>Hello World!</h1>
 
-    const [inputValues, setInputValues] = useState(["", "", ""])
-    const [inputValues1, setInputValues1] = useState(["", "", ""])
+			<input
+				type="text"
+				value={inputValues1[0]}
+				onChange={(e) => {
+					setInputValues1([
+						e.currentTarget.value,
+						inputValues1[1],
+						inputValues1[2],
+					]);
+				}}></input>
+			<input
+				type="text"
+				value={inputValues1[1]}
+				onChange={(e) => {
+					setInputValues1([
+						inputValues1[0],
+						e.currentTarget.value,
+						inputValues1[2],
+					]);
+				}}></input>
+			<input
+				type="text"
+				value={inputValues1[2]}
+				onChange={(e) => {
+					setInputValues1([
+						inputValues1[0],
+						inputValues1[1],
+						e.currentTarget.value,
+					]);
+				}}></input>
 
-    return (
-        <main className={styles.main}>
-            <Navbar></Navbar>
-            <h1>Hello World!</h1>
+			<button
+				onClick={() => {
+					createProject(
+						inputValues1[0],
+						inputValues1[1],
+						inputValues1[2]
+					);
+				}}>
+				Create new project (id, name, description)
+			</button>
 
-            <input type="text" value={inputValues1[0]} onChange={(e) => {setInputValues1([e.currentTarget.value, inputValues1[1], inputValues1[2]])}}></input>
-            <input type="text" value={inputValues1[1]} onChange={(e) => {setInputValues1([inputValues1[0], e.currentTarget.value, inputValues1[2]])}}></input>
-            <input type="text" value={inputValues1[2]} onChange={(e) => {setInputValues1([inputValues1[0], inputValues1[1], e.currentTarget.value])}}></input>
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
 
-            <button onClick={() => {createProject(inputValues1[0], inputValues1[1], inputValues1[2])}}>
-                Create new project (id, name, description)
-            </button>
+			<input
+				type="text"
+				value={inputValues[0]}
+				onChange={(e) => {
+					setInputValues([
+						e.currentTarget.value,
+						inputValues[1],
+						inputValues[2],
+					]);
+				}}></input>
+			<input
+				type="text"
+				value={inputValues[1]}
+				onChange={(e) => {
+					setInputValues([
+						inputValues[0],
+						e.currentTarget.value,
+						inputValues[2],
+					]);
+				}}></input>
+			<input
+				type="text"
+				value={inputValues[2]}
+				onChange={(e) => {
+					setInputValues([
+						inputValues[0],
+						inputValues[1],
+						e.currentTarget.value,
+					]);
+				}}></input>
 
-            <br/>
-            <br/>
-            <br/>            
-            <br/>
-            <br/>
-            <br/>
+			<button
+				onClick={() => {
+					createIssue(inputValues[0], inputValues[1], inputValues[2]);
+				}}>
+				Create new issue (title, description, projectId)
+			</button>
 
-            <input type="text" value={inputValues[0]} onChange={(e) => {setInputValues([e.currentTarget.value, inputValues[1], inputValues[2]])}}></input>
-            <input type="text" value={inputValues[1]} onChange={(e) => {setInputValues([inputValues[0], e.currentTarget.value, inputValues[2]])}}></input>
-            <input type="text" value={inputValues[2]} onChange={(e) => {setInputValues([inputValues[0], inputValues[1], e.currentTarget.value])}}></input>
+			<br />
 
-            <button onClick={() => {createIssue(inputValues[0], inputValues[1], inputValues[2])}}>
-                Create new issue (title, description, projectId)
-            </button>
+			<br />
+			<br />
+			<br />
 
-            <br/>
+			<select
+				onChange={(e) => {
+					fetchIssues(e.currentTarget.value);
+					// might be buggy since we're using the same state as below for the search, but will be seperated anyways, so don't care
+				}}>
+				<option key={0} value={""}></option>
+				{projects.map((project, index) => (
+					<option key={index + 1} value={project.id}>{`${
+						project.data().name
+					} (${project.id})`}</option>
+				))}
+			</select>
 
-            <br/>
-            <br/>
+			<select
+				onChange={(e) => {
+					setIssue(e.currentTarget.value);
+				}}>
+				<option key={0} value={""}></option>
+				{issues.map((issue, index) => (
+					<option key={index + 1} value={issue.id}>{`${
+						issue.data().title
+					} (${issue.id})`}</option>
+				))}
+			</select>
 
-            <button onClick={() => {
-                signOut(auth).then((a: any) => {
-                    console.log(a + "you have been signed out")
-                })
+			<input
+				type="datetime-local"
+				onChange={(e) => {
+					if (e.currentTarget.valueAsDate) {
+						setSelectedDate(e.currentTarget.valueAsDate);
+					}
+				}}></input>
 
-            }}> log out </button>
+			<input
+				type="number"
+				onChange={(e) => {
+					if (e.currentTarget.valueAsNumber) {
+						setSelectedDuration(e.currentTarget.valueAsNumber);
+					}
+				}}></input>
 
-            <br/>
-            <br/>
+			<button
+				onClick={() => {
+					createWorklog(issue, selectedDate, selectedDuration);
+				}}>
+				Add worklog
+			</button>
 
+			<br />
+			<br />
+			<br />
 
-            <select onChange={(e) => {
-                setSelectedProject(e.currentTarget.value);
-            }} onMouseEnter={(e) => {
-                setSelectedProject(e.currentTarget.value);
-            }}>
-                {projects.map((project, index) => (
-                    <option key={index} value={project.id}>{`${project.data().name} (${project.id})`}</option>
-                ))}
-                
-            </select>
+			<br />
+			<br />
 
-            <button onClick={() => {
-                console.log(selectedProject);
-                fetchIssues(selectedProject);
-            }}>Get all issues</button>
+			<button
+				onClick={() => {
+					signOut(auth).then((a: any) => {
+						console.log(a + "you have been signed out");
+					});
+				}}>
+				{" "}
+				log out{" "}
+			</button>
 
-            {/* Map over the issues array and render a div for each issue */}
-            {issues.map((issue, index) => (
-                <div key={issue.id}>
-                    {issue.data().title}
-                    <div style={{fontSize: "0.8em" }}>{issue.data().description}</div>    
-                </div>
-            ))}
-        </main>
-    )
+			<br />
+			<br />
+
+			<select
+				onChange={(e) => {
+					setSelectedProjectForSearch(e.currentTarget.value);
+				}}>
+				<option key={0} value={""}></option>
+				{projects.map((project, index) => (
+					<option key={index + 1} value={project.id}>{`${
+						project.data().name
+					} (${project.id})`}</option>
+				))}
+			</select>
+
+			<button
+				onClick={() => {
+					fetchIssues(selectedProjectForSearch);
+					// TODO: check if the field is not empty
+				}}>
+				Get all issues
+			</button>
+
+			{/* Map over the issues array and render a div for each issue */}
+			{issues.map((issue, index) => (
+				<div key={issue.id}>
+					{issue.data().title}
+					<div style={{ fontSize: "0.8em" }}>
+						{issue.data().description}
+					</div>
+				</div>
+			))}
+		</main>
+	);
 }
