abdf7a07b1d4f387680b754de1768977373ae46a
SchneiderMaster <fynn2746@outlook.de>
2024-04-21 14:28:10 +0200

    made the timetable better

--- src/pages/timetable.tsx
+++ src/pages/timetable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Ref, useEffect, useRef, useState } from "react";
 import styles from "@/styles/timetable.module.css";
 import "@/styles/globals.css";
 import Navbar from "@/app/navbar";
@@ -10,13 +10,72 @@ export default function TimeTable() {
 		}`;
 	}
 
+	const [worklogDivs, setWorklogDivs] = useState<JSX.Element[]>([]);
+
+	const [dragging, setDragging] = useState<boolean>(false);
+
+	function createSingleDiv(left: number, top: number) {
+		let tempDivs = [...worklogDivs];
+		tempDivs.push(
+			<div
+				style={{
+					position: "absolute",
+					top: `${top}px`,
+					left: `${left}px`,
+					width: "200px",
+					height: `${15 * 1}px`,
+					backgroundColor: "#5e27cc",
+					zIndex: 100,
+					overflow: "hidden",
+				}}>
+				TE-0
+			</div>
+		);
+		setWorklogDivs(tempDivs);
+	}
+
+	useEffect(() => {
+		const whileDragging = () => {
+			const intervalId = setInterval(() => {
+				console.log("dragging");
+				if (!dragging) {
+					clearInterval(intervalId);
+				}
+			}, 10);
+			return () => clearInterval(intervalId);
+		};
+
+		if (dragging) {
+			whileDragging();
+		}
+	}, [dragging]);
+
 	function generateDivs(): JSX.Element[] {
 		const divs: JSX.Element[] = [];
 		const totalDivs = 96 * 7; // Total number of divs required
 
 		for (let i = 0; i < totalDivs; i++) {
 			divs.push(
-				<div className={styles.cell} key={i}>
+				<div
+					className={`${styles.cell} ${
+						Math.floor(i / 7) % 4 === 0 ? styles.fullHour : ""
+					}`}
+					key={i}
+					onMouseDown={(e) => {
+						setDragging(true);
+						let pos = e.currentTarget.getBoundingClientRect();
+						createSingleDiv(pos.left, pos.top);
+
+						console.log(pos.x, pos.y);
+					}}
+					onMouseUp={() => {
+						console.log("up");
+						setDragging(false);
+					}}
+					// onMouseDown={(e) => {
+					// 	console.log(e.clientX, e.clientY);
+					// }}
+				>
 					<div className={styles.cellText}>
 						{getTime(Math.floor(i / 7) * 15)}
 					</div>
@@ -32,10 +91,14 @@ export default function TimeTable() {
 			<Navbar></Navbar>
 			<div>Also Hi, lol</div>
 
+			{worklogDivs.map((worklogDiv, index) => {
+				return worklogDiv;
+			})}
+
 			<div className={styles.table}>
 				{generateDivs()}
 
-				<div
+				{/* <div
 					style={{
 						backgroundColor: "#aaa",
 						gridArea: "4 / 2 / 12 / 2",
@@ -57,7 +120,7 @@ export default function TimeTable() {
 						padding: "5px 7px 7px 7px",
 					}}>
 					TE-1
-				</div>
+				</div> */}
 			</div>
 		</main>
 	);
